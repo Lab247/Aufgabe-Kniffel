5f1466f73ed460e3f78dac542d03c2e2ebec9cae
Lab247 <42779897+Lab247@users.noreply.github.com>
2018-09-14 09:33:26 +0200

    Add files via upload
    added Sortingtests

--- unit1.pas
+++ unit1.pas
@@ -14,12 +14,23 @@ type
 
   TfrmDice = class(TForm)
     btnPlay: TButton;
+    btnZufallszahlen: TButton;
+    btnBubbleSort: TButton;
+    edtAnzahl: TEdit;
+    lblBubbleSort: TLabel;
+    lblAnzahl: TLabel;
+    lblUnsortiert: TLabel;
+    ListBoxBubbleSort: TListBox;
+    ListBoxUnsortiert: TListBox;
+    pnlSortHead: TPanel;
     pnl1: TPanel;
     pnl2: TPanel;
     pnl3: TPanel;
     pnl4: TPanel;
     pnl5: TPanel;
+    procedure btnBubbleSortClick(Sender: TObject);
     procedure btnPlayClick(Sender: TObject);
+    procedure btnZufallszahlenClick(Sender: TObject);
   private
 
   public
@@ -28,6 +39,8 @@ type
 
 var
   frmDice: TfrmDice;
+  Anzahl : Integer;
+  ArrayZahlen : array of Integer; //offenes Array für Zahlen
 
 implementation
 
@@ -66,6 +79,38 @@ begin
   pnl3.caption := IntToStr(dice[3]);
   pnl4.caption := IntToStr(dice[4]);
   pnl5.caption := IntToStr(dice[5]);
+
+end;
+
+procedure TfrmDice.btnBubbleSortClick(Sender: TObject);
+var i,j,Merker : integer;
+begin
+     for i:=Anzahl downto 1 do
+         for j:=0 to i-1 do
+             if ArrayZahlen[j] > ArrayZahlen[j+1] then
+                begin
+                     Merker := ArrayZahlen[j];
+                     ArrayZahlen[j] := ArrayZahlen[j+1];
+                     ArrayZahlen[j+1] := Merker;
+                end;
+     for i:=0 to Anzahl-1 do
+         ListBoxBubbleSort.Items.Insert(i,IntToStr(ArrayZahlen[i]));
+end;
+
+procedure TfrmDice.btnZufallszahlenClick(Sender: TObject);
+var i : Integer;
+
+begin
+     Anzahl := StrToInt(edtAnzahl.Text); //Anzahl auslesen
+     setLength(ArrayZahlen,Anzahl);      //Array auf Größe Anzahl setzen
+     randomize;                          //random init
+     for i := 0 to Anzahl-1 do           //Schleife füllen des arrays
+         Begin
+           //Zufallszahl in Array
+           ArrayZahlen[i] := Random(Anzahl)+1;
+           //Zahl in Listbox schreiben
+           ListBoxUnsortiert.Items.Insert(i,IntToStr(ArrayZahlen[i]));
+         end;
 
 end;
 
